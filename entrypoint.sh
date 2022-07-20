71328904be01d11b11e542dc105b8ce2fb1d2c38
sarjerao tambe <sarjutambe@gmail.com>
2022-07-20 16:53:45 +0530

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,61 +1,191 @@
 #!/bin/bash
 
-# Config
-default_semvar_bump=${DEFAULT_BUMP:-minor}
-dry_run_mode=${DRY_RUN:-false}
+set -o pipefail
 
-# get latest tag
-tag=$(git describe --tags `git rev-list --tags --max-count=1`)
-tag_commit=$(git rev-list -n 1 $tag)
+# config
+default_semvar_bump=${DEFAULT_BUMP:-minor}
+with_v=${WITH_V:-false}
+release_branches=${RELEASE_BRANCHES:-master,main}
+custom_tag=${CUSTOM_TAG}
+source=${SOURCE:-.}
+dryrun=${DRY_RUN:-false}
+initial_version=${INITIAL_VERSION:-0.0.0}
+tag_context=${TAG_CONTEXT:-repo}
+suffix=${PRERELEASE_SUFFIX:-beta}
+verbose=${VERBOSE:-true}
+verbose=${VERBOSE:-true}
+# since https://github.blog/2022-04-12-git-security-vulnerability-announced/ runner uses?
+git config --global --add safe.directory /github/workspace
+
+cd ${GITHUB_WORKSPACE}/${source}
+
+echo "*** CONFIGURATION ***"
+echo -e "\tDEFAULT_BUMP: ${default_semvar_bump}"
+echo -e "\tWITH_V: ${with_v}"
+echo -e "\tRELEASE_BRANCHES: ${release_branches}"
+echo -e "\tCUSTOM_TAG: ${custom_tag}"
+echo -e "\tSOURCE: ${source}"
+echo -e "\tDRY_RUN: ${dryrun}"
+echo -e "\tINITIAL_VERSION: ${initial_version}"
+echo -e "\tTAG_CONTEXT: ${tag_context}"
+echo -e "\tPRERELEASE_SUFFIX: ${suffix}"
+echo -e "\tVERBOSE: ${verbose}"
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+pre_release="true"
+IFS=',' read -ra branch <<< "$release_branches"
+for b in "${branch[@]}"; do
+    echo "Is $b a match for ${current_branch}"
+    if [[ "${current_branch}" =~ $b ]]
+    then
+        pre_release="false"
+    fi
+done
+echo "pre_release = $pre_release"
+
+# fetch tags
+git fetch --tags
+    
+tagFmt="^v?[0-9]+\.[0-9]+\.[0-9]+$" 
+preTagFmt="^v?[0-9]+\.[0-9]+\.[0-9]+(-$suffix\.[0-9]+)?$" 
+
+# get latest tag that looks like a semver (with or without v)
+case "$tag_context" in
+    *repo*) 
+        taglist="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$tagFmt")"
+        tag="$(semver $taglist | tail -n 1)"
+
+        pre_taglist="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$preTagFmt")"
+        pre_tag="$(semver "$pre_taglist" | tail -n 1)"
+        ;;
+    *branch*) 
+        taglist="$(git tag --list --merged HEAD --sort=-v:refname | grep -E "$tagFmt")"
+        tag="$(semver $taglist | tail -n 1)"
+
+        pre_taglist="$(git tag --list --merged HEAD --sort=-v:refname | grep -E "$preTagFmt")"
+        pre_tag=$(semver "$pre_taglist" | tail -n 1)
+        ;;
+    * ) echo "Unrecognised context"; exit 1;;
+esac
+
+
+# if there are none, start tags at INITIAL_VERSION which defaults to 0.0.0
+if [ -z "$tag" ]
+then
+    log=$(git log --pretty='%B')
+    tag="$initial_version"
+    if [ -z "$pre_tag" ] && $pre_release
+    then
+      pre_tag="$initial_version"
+    fi
+else
+    log=$(git log $tag..HEAD --pretty='%B')
+fi
 
 # get current commit hash for tag
+tag_commit=$(git rev-list -n 1 $tag)
+
+# get current commit hash
 commit=$(git rev-parse HEAD)
 
 if [ "$tag_commit" == "$commit" ]; then
     echo "No new commits since previous tag. Skipping..."
+    echo ::set-output name=tag::$tag
     exit 0
 fi
 
-# if there are none, start tags at 0.0.0
-if [ -z "$tag" ]
+# echo log if verbose is wanted
+if $verbose
 then
-    log=$(git log --pretty=oneline)
-    t=0.0.0
-else
-    log=$(git log $tag..HEAD --pretty=oneline)
+  echo $log
 fi
 
-# get commit logs and determine home to bump the version
-# supports #major, #minor, #patch (anything else will be '$default_semvar_bump')
 case "$log" in
-    *#skip* ) echo "#skip tag present. Skipping version"; exit 0 ;;
-    *#major* ) new=$(semver bump major $tag);;
-    *#minor* ) new=$(semver bump minor $tag);;
-    *#patch* ) new=$(semver bump patch $tag);;
-    *hot-fix* ) new=$(semver bump patch $tag);;
-    *hotfix* ) new=$(semver bump patch $tag);;
-    * ) new=$(semver bump `echo $default_semvar_bump` $tag);;
+    *#major* ) new=$(semver -i major $tag); part="major";;
+    *#minor* ) new=$(semver -i minor $tag); part="minor";;
+    *#patch* ) new=$(semver -i patch $tag); part="patch";;
+    *#none* ) 
+        echo "Default bump was set to none. Skipping..."; echo ::set-output name=new_tag::$tag; echo ::set-output name=tag::$tag; exit 0;;
+    * ) 
+        if [ "$default_semvar_bump" == "none" ]; then
+            echo "Default bump was set to none. Skipping..."; echo ::set-output name=new_tag::$tag; echo ::set-output name=tag::$tag; exit 0 
+        else 
+            new=$(semver -i "${default_semvar_bump}" $tag); part=$default_semvar_bump 
+        fi 
+        ;;
 esac
 
-echo $new
-
-dt=$(date '+%Y-%m-%dT%H:%M:%SZ')
-remote=$(git config --get remote.origin.url)
-repo=$(basename $remote .git)
-
-if [ "$dry_run_mode" = true ] ; then
-    echo "[DRY-RUN] New version tag is: $new"
-    exit 0
+if $pre_release
+then
+    # Already a prerelease available, bump it
+    if [[ "$pre_tag" == *"$new"* ]]; then
+        new=$(semver -i prerelease $pre_tag --preid $suffix); part="pre-$part"
+    else
+        new="$new-$suffix.1"; part="pre-$part"
+    fi
 fi
 
-echo "$dt: **pushing tag $new to repo $REPO_OWNER/$repo"
+echo $part
 
-curl -s -X POST https://api.github.com/repos/$REPO_OWNER/$repo/git/refs \
+# prefix with 'v'
+if $with_v
+then
+	new="v$new"
+fi
+
+if [ ! -z $custom_tag ]
+then
+    new="$custom_tag"
+fi
+
+if $pre_release
+then
+    echo -e "Bumping tag ${pre_tag}. \n\tNew tag ${new}"
+else
+    echo -e "Bumping tag ${tag}. \n\tNew tag ${new}"
+fi
+
+# set outputs
+echo ::set-output name=new_tag::$new
+echo ::set-output name=part::$part
+
+# use dry run to determine the next tag
+if $dryrun
+then
+    echo ::set-output name=tag::$tag
+    exit 0
+fi 
+
+echo ::set-output name=tag::$new
+
+# create local git tag
+git tag $new
+
+# push new tag ref to github
+dt=$(date '+%Y-%m-%dT%H:%M:%SZ')
+full_name=$GITHUB_REPOSITORY
+git_refs_url=$(jq .repository.git_refs_url $GITHUB_EVENT_PATH | tr -d '"' | sed 's/{\/sha}//g')
+
+echo "$dt: **pushing tag $new to repo $full_name"
+
+git_refs_response=$(
+curl -s -X POST $git_refs_url \
 -H "Authorization: token $GITHUB_TOKEN" \
 -d @- << EOF
-
 {
   "ref": "refs/tags/$new",
   "sha": "$commit"
 }
 EOF
+)
+
+git_ref_posted=$( echo "${git_refs_response}" | jq .ref | tr -d '"' )
+
+echo "::debug::${git_refs_response}"
+if [ "${git_ref_posted}" = "refs/tags/${new}" ]; then
+  exit 0
+else
+  echo "::error::Tag was not created properly."
+  exit 1
+fi
